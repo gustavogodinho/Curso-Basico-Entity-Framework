86e44124313daac8fb17f85abec465a2820d0c43
Gustavo Gilberto Godinho <11860941+gustavogodinho@users.noreply.github.com>
2019-05-16 17:49:24 -0300

    busca e insert para aluno

---
+++ Entity.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Entity_CursoBasico
+{
+    public class Entity
+    {
+        public static string GravaAluno(string nome)
+        {
+            try
+            {
+                using (var ctx = new EscolaEntity())
+                {
+                    ctx.Aluno.Add(new Aluno() { AlunoNome = nome });
+                    ctx.SaveChanges();
+                }
+                return "Aluno: " + nome + " adicionado com sucesso!";
+            }
+            catch (Exception)
+            {
+                return "Deu erro ta ligado.";
+            }
+        }
+
+        public static string GravaPadrao(string padraoNome, string descricao)
+        {
+            try
+            {
+                using (var ctx = new EscolaEntity())
+                {
+                    ctx.Padrao.Add(new Padrao() { PadraoNome = padraoNome, Descricao = descricao });
+                    ctx.SaveChanges();
+                }
+                return "Padrao: " + padraoNome + " adicionado com sucesso!";
+            }
+            catch (Exception ex)
+            {
+                return "Deu erro ta ligado. " + ex.Message;
+            }
+
+        }
+
+        public static Aluno ObterAluno(string nome)
+        {
+
+            try
+            {
+                using (var ctx = new EscolaEntity())
+                {
+                    var listaAluno = ctx.Aluno.ToList();
+
+                    Aluno aluno = ctx.Aluno
+                                   .Where(s => s.AlunoNome.Contains(nome))
+                                   .FirstOrDefault();
+                    if (aluno == null)
+                    {
+                        throw new Exception("procure o aluno correto, Rodrigo Maia");
+                    }
+                    return aluno;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_CursoBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite:");
+            Console.WriteLine(" 1 para cadastrar um Aluno");
+            Console.WriteLine(" ");
+            Console.WriteLine(" 2 para cadastrar um Padrão");
+            Console.WriteLine(" ");
+            Console.WriteLine(" 3 para cadastrar um Padrão");
+            Console.WriteLine(" ");
+            string cadastro = Console.ReadLine();
+
+            if (cadastro == "1")
+            {
+                Console.WriteLine("Digite o nome");
+                string nombre = Console.ReadLine();
+                var teste = Entity.GravaAluno(nombre);
+                Console.WriteLine(teste);
+            }
+            else if (cadastro == "2")
+            {
+                Console.WriteLine("Digite o nome");
+                string padraoNome = Console.ReadLine();
+                Console.WriteLine("Digite a descrição");
+                string descricao = Console.ReadLine();
+                var padrao = Entity.GravaPadrao(padraoNome, descricao);
+                Console.WriteLine(padrao);
+            }
+            else if (cadastro == "3")
+            {
+                Console.WriteLine("Digite o nome");
+                string buscaNome = Console.ReadLine();
+
+                try
+                {
+                    var aluno = Entity.ObterAluno(buscaNome);
+
+                    if (aluno != null)
+                    {
+                        Console.WriteLine(aluno.AlunoNome);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"O Aluno {buscaNome} não foi encontrado!");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Voce nao sabe procura nem um aluno: {ex.Message} ");
+                }
+            }
+        }
+    }
+}
